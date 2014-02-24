c7f3a95596e2d1be3f2baeb28bf90f4f781b9e4b
Martin Wetterwald <martin.wetterwald@insa-lyon.fr>
2014-02-24 10:41:53 +0100

    Enhances base template with use of bootstrap.

--- src/app.py
+++ src/app.py
@@ -5,6 +5,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/map")
 def map():
     return render_template("map.html")
 
