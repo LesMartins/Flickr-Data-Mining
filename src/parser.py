c897771d00677844c085822f5c5f86074f615efc
Martin Wetterwald <martin.wetterwald@insa-lyon.fr>
2014-03-10 09:13:00 +0100

    Adds GMaps generation.

--- src/parser.py
+++ src/parser.py
@@ -4,6 +4,7 @@
 import mongoengine
 import config
 import csv
+import pymaps
 
 def initdb(db):
     db.drop_database(config.db_name)
@@ -20,7 +21,12 @@ class FlickrDialect(csv.Dialect):
 def parse():
     data = csv.DictReader(open(config.csv_file_name, "rb"), dialect=FlickrDialect())
 
+def genMap():
+    tmap = pymaps.Map()
+    tmap.zoom = 3
+    gmap = pymaps.PyMap(maplist=[tmap])
+    return gmap.pymapjs()
+
 if __name__ == "__main__":
     db = mongoengine.connect(config.db_name)
     initdb(db)
-    parse()
--- src/web.py
+++ src/web.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask, render_template
+import parser
 
 app = Flask(__name__)
 
@@ -10,7 +11,9 @@ def home():
     return render_template("index.html")
 
 @app.route("/map")
-    return render_template("map.html")
+def map():
+    the_map = parser.genMap()
+    return render_template("map.html", the_map=the_map)
 
 if __name__ == "__main__":
     app.run(debug=True)
