1597d132be8742166b34def0e707288882443e88
Martin Wetterwald <martin.wetterwald@insa-lyon.fr>
2014-03-22 20:15:08 +0100

    Adds markers on each cluster's center.

--- src/gmaps.py
+++ src/gmaps.py
@@ -20,14 +20,32 @@ class GMaps:
                 center: mycenter
             }
             var theMap = new google.maps.Map(document.getElementById('%s'), options);
+
+            %s
         }
         google.maps.event.addDomListener(window, 'load', init);
     </script>
-    """ % (str(self.center), self.zoom, self.div_id)
+    """ % (str(self.center), self.zoom, self.div_id, self._points())
+
+    def _points(self):
+        string = ''
+        for point in self.points:
+            string += """
+            new google.maps.Marker({
+                position: new google.maps.LatLng %s,
+                map: theMap
+            });""" % (str(point))
+
+        return string
+
 
 
 def genMap(centers, data):
     myMap = GMaps()
     myMap.center = (45.75972, 4.84222)
     myMap.zoom = 11
+
+    for i, row in enumerate(centers):
+        myMap.points.append((float(row['x']), float(row['y'])))
+
     return myMap.js()
